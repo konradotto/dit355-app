9baa73084a4ec4ec542b6b2f734cc0a9b85e0546
ih8c01d <jeanpaull@live.it>
2019-11-07 11:46:38 +0100

    initial commit

---
+++ DIT355/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DIT355
+//
+//  Created by Jean paul Massoud on 2019-11-07.
+//  Copyright © 2019 DIT355-group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
